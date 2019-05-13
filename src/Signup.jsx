368ab542647d6b7e62d458ca74b74366c92a4c92
Sarah Fogalin <sarah_fogalin@hotmail.com>
2019-05-13 18:32:55 -0400

    signup login and logout components made

---
+++ src/Signup.jsx
@@ -0,0 +1,89 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+
+class UnconnectedSignup extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { username: "", password: "", email: "" };
+  }
+
+  handleUsernameChange = event => {
+    console.log("new username: ", event.target.value);
+    this.setState({ username: event.target.value });
+  };
+
+  handlePasswordChange = event => {
+    console.log("new password: ", event.target.value);
+    this.setState({ password: event.target.value });
+  };
+
+  handleEmailChange = event => {
+    console.log("new email: ", event.target.value);
+    this.setState({ email: event.target.value });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    let data = new FormData();
+    data.append("username", this.state.username);
+    data.append("password", this.state.password);
+    data.append("email", this.state.email);
+    fetch("http://localhost:4000/signup", { method: "POST", body: data })
+      .then(response => {
+        return response.text();
+      })
+      .then(ResponseBody => {
+        let body = JSON.parse(ResponseBody);
+        if (!body.success) {
+          alert("signup fail");
+          return;
+        }
+        this.props.dispatch({ type: "signup-success" });
+        return fetch("http://localhost:4000/login", {
+          method: "POST",
+          body: data,
+          credentials: "include"
+        })
+          .then(response => {
+            return response.text();
+          })
+          .then(ResponseBody => {
+            let body = JSON.parse(ResponseBody);
+            if (!body.success) {
+              alert("login failed..");
+              return;
+            }
+            this.props.dispatch({ type: "login-success" });
+          });
+      });
+  };
+
+  render = () => {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            onChange={this.handleUsernameChange}
+            placeholder="Username"
+          />
+          <input
+            type="text"
+            onChange={this.handlePasswordChange}
+            placeholder="Password minimum 8 characters"
+          />
+          <input
+            type="text"
+            onChange={this.handleEmailChange}
+            placeholder="Email Addresss"
+          />
+          <input type="submit" value="Sign up!" />
+        </form>
+      </div>
+    );
+  };
+}
+
+let Signup = connect()(UnconnectedSignup);
+
+export default Signup;
--- src/store.jsx
+++ src/store.jsx
@@ -1,5 +1,18 @@
 import { createStore } from "redux";
 
+let reducer = (state, action) => {
+  if (action.type === "signup-success") {
+    return { ...state, loggedIn: true };
+  }
+  if (action.type === "login-success") {
+    return { ...state, loggedIn: true };
+  }
+  if (action.type === "logout") {
+    return { ...state, loggedIn: false };
+  }
+  return state;
+};
+
 const store = createStore(
   reducer,
   { loggedIn: false, results: [], username: "" },
